9b57531c8d165a2ac5c048020a45ad0e698953c4
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-21 16:25:56 -0300

    Header component.

---
+++ src/components/header.rs
@@ -0,0 +1,35 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+pub struct Header {
+    pub user_logged: bool,
+}
+
+pub enum HeaderMsg {
+    Exit,
+}
+
+impl Component for Header {
+    type Message = HeaderMsg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    // TODO: fn update()
+    
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+        html! {
+            <header id="main-header">
+                <h1>{"User Manager"}</h1>
+                if self.user_logged {
+                    <button id="btn-exit">
+                        <img src="assets/img/bx-exit.svg" alt="Exit"/>
+                    </button>
+                }
+            </header>
+        }
+    }
+}
