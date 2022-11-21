ac62496d16877a6ee70ae910df1f121d26fa7f25
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-21 16:31:18 -0300

    Include header to main structure.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 use user_manager::routes::Route;
 use user_manager::routes::switch;
+use user_manager::components::{
+    header::Header,
+};
 
 #[function_component(App)]
 fn app() -> Html {
     html! {
         <BrowserRouter>
+            <Header/>
             <div id="main-container">
                 <Switch<Route> render={Switch::render(switch)}/>
             </div>
