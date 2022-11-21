9ea0a3240ba86abfe0d46f5299b24ea6079ddcb7
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-21 16:07:37 -0300

    Main structure.

--- src/main.rs
+++ src/main.rs
@@ -1,147 +1,15 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+use crate::routes::Route;
 
-/* Messages */
-enum MsgApp {
-    ListUsers,
-    AddUser,
-}
-enum MsgList {
-    RemoveItem,
-}
-enum MsgFormAddUser {
-    AddedUser,
-}
-
-/* Components */
-struct App {
-    section: u8,
-}
-struct Header;
-struct ListItem {
-    id: u8,
-    name: String,
-}
-struct List {
-    items: Vec<ListItem>,
-}
-struct FormAddUser;
-
-/* Properties */
-#[derive(Properties, PartialEq)]
-struct HeaderProps {
-    children: Children,
-}
-
-/* Implementations */
-impl Component for App {
-    type Message = MsgApp;
-    type Properties = ();
-    
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            section: 0
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            MsgApp::ListUsers => {
-                self.section = 0;
-                true
-            },
-            MsgApp::AddUser => {
-                self.section = 1;
-                true
-            },
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <>
-                <Header>
-                    <div class="logo-area">
-                        <h1>{"Compraqui"}</h1>
-                    </div>
-                    <div class="options">
-                        <button onclick={ctx.link().callback(|_| MsgApp::ListUsers)}>{"List Users"}</button>
-                        <button onclick={ctx.link().callback(|_| MsgApp::AddUser)}>{"Add User"}</button>
-                    </div>
-                    if self.section == 0 {
-                        <List />
-                    } else {
-                        <FormAddUser />
-                    }
-                </Header>
-            </>
-        }
-    }
-}
-
-impl Component for Header {
-    type Message = ();
-    type Properties = HeaderProps;
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            { for ctx.props().children.iter() }
-        }
-    }
-}
-
-impl Component for List {
-    type Message = MsgList;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self {
-            // items: Vec::<ListItem>::new()
-            items: vec![
-                ListItem {
-                    id: 1,
-                    name: "Gabriel".to_string(),
-                },
-            ]
-        }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            MsgList::RemoveItem => {
-                self.items.pop();
-                true
-            }
-        }
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        self.items.iter().map(|item| {
-            html! {
-                <p>{format!("{} - {}", item.id, item.name)}</p>
-            }
-        }).collect()
-    }
-}
-
-impl Component for FormAddUser {
-    type Message = MsgFormAddUser;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self
-    }
-
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <form method="POST">
-                <input type="text" placeholder="Name"/>
-                <input type="submit" value="Add"/>
-            </form>
-        }
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <div id="main-container">
+                <Switch<Route> render={Switch::render(switch)}/>
+            </div>
+        </BrowserRouter>
     }
 }
 
